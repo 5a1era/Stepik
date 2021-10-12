ef19c00dab85bc8298fff875e25850ace366dae4
Valera Malyshov <valera.malyshov@gmail.com>
2021-10-12 20:55:50 +0300

    My first commit IntellijIdea

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String [] args) {
+        ArrayList<Integer> arrayList1 = new ArrayList<>();
+        ArrayList<Integer> arrayList2 = new ArrayList<>();
+        ArrayList<Integer> arrayList3 = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
