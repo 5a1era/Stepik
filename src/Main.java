ed3d6bb3013df7cb40416667d9c56fc03aa8d022
Valera Malyshov <valera.malyshov@gmail.com>
2021-10-12 23:19:02 +0300

    Filled three ArrayLists from the console

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,27 @@ public class Main {
         ArrayList<Integer> arrayList2 = new ArrayList<>();
         ArrayList<Integer> arrayList3 = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
+        String[] str;
+        for (int i = 0; i < 3; i++) {
+            str = scanner.nextLine().split(" ");
+            for (String s : str) {
+                switch (i) {
+                    case 0: arrayList1.add(Integer.valueOf(s));
+                    break;
+                    case 1: arrayList2.add(Integer.valueOf(s));
+                    break;
+                    case 2: arrayList3.add(Integer.valueOf(s));
+                    break;
+                }
+            }
+
+        }
+
+        System.out.print(arrayList1 + "-" + arrayList2 + "-" + arrayList3);
+
+
+
+
 
     }
 }
